3ea36f9db6041c659715deb2af79b4eab0276a97
Khaled Soudy <khaledsoudy2012@gmail.com>
2024-10-06 04:09:06 +0300

    Find and extract the first quote and its author.

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://quotes.toscrape.com/'
 
-# Make a GET request
-response = requests.get(url)
+def get_quotes():
+    url = 'https://quotes.toscrape.com/'
+    
+    # Make a GET request
+    response = requests.get(url)
+    
+    # Make a soup object
+    soup = BeautifulSoup(response.content, 'lxml')
+    
+    # Find and extract the first quote.
+    quote = soup.find('span', class_='text').text
+    print(quote)
+    
+    # Find and extract the author's name of the first quote
+    author = soup.find('small', class_='author').text
+    print(author)
 
-# Check response status code
-print(response.status_code)          # OUTPUT: 200
 
-# Display the result
-print(response.text)
+if __name__ == '__main__':
+    get_quotes()
+
+# OUTPUT:
+# “The world as we have created it is a process of our thinking. It cannot be changed without changing our thinking.”
+# Albert Einstein
